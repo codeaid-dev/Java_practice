598a4d7198d0d6c30ae70b67b31cbddcf8e4daa4
codeaid-dev <codeaid.lab@gmail.com>
2020-10-18 15:00:41 +0900

    fix

--- IPHostAddress/IPHostAddress.java
+++ IPHostAddress/IPHostAddress.java
@@ -7,26 +7,35 @@ public class IPHostAddress {
       InetAddress inet  = InetAddress.getLocalHost();
       //InetAddress inet = InetAddress.getByName("xxx.xxx.xxx.xxx");
 
-      String ip = inet.getHostAddress();//IPアドレス
+      String ip = inet.getHostAddress();
       System.out.println("IP Address: " + ip);
-      String hostname = inet.getHostName();    //ホスト名
+      String hostname = inet.getHostName();
       System.out.println("Host Name: " + hostname);
-
+      System.out.println("");
 
       } catch (Exception e) {
-          System.out.println("Unable to find address for " + e.getMessage);
+          //System.out.println("Unable to find address for " + e.getMessage);
           e.printStackTrace();
       }
 
       try {
-        Enumeration <NetworkInterface> interfaceList;//集合内の列挙操作に使う古い型
+        Enumeration <NetworkInterface> interfaceList;
         interfaceList = NetworkInterface.getNetworkInterfaces();
-        while(interfaceList.hasMoreElements()){//すべてのインターフェイスを走査
-          NetworkInterface nInterface = (NetworkInterface)interfaceList.nextElement();
-          System.out.print("  " + nInterface.getName() );//ネットワーク識別名
+        while(interfaceList.hasMoreElements()){
+          NetworkInterface nInterface = interfaceList.nextElement();
+          System.out.println("Interface " + nInterface.getName() + ": ");
+          Enumeration <InetAddress> addrList = nInterface.getInetAddresses();
+          if (!addrList.hasMoreElements()) continue;
+          while(addrList.hasMoreElements()) {
+            InetAddress address = addrList.nextElement();
+            //System.out.print("\tAddress " + ((address instanceof Inet4Address ? "(IPv4)"
+            //                                : (address instanceof Inet6Address ? "(IPv6)" : "(?)"))));
+            System.out.println(": " + address.getHostAddress());
+          }
         }
       } catch (SocketException se) {
-      System.out.println("Error getting network interfaces: " + se.getMessage);
+        //System.out.println("Error getting network interfaces: " + se.getMessage);
+        se.printStackTrace();
     }
   }
 }
