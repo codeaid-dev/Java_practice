aa81df41a3c4d7169370dd3cc5694545933d17a8
codeaid-dev <codeaid.lab@gmail.com>
2020-11-20 20:09:00 +0900

    fix add

--- UDP/UDPServerFile.java
+++ UDP/UDPServerFile.java
@@ -1,8 +1,10 @@
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 
-public class UDPServer {
+public class UDPServerFile {
   public static void main(String[] args) throws IOException{
     int port = Integer.parseInt(args[0]);
     DatagramSocket socket = new DatagramSocket(port); // UDP受信用ソケット構築
@@ -13,7 +15,17 @@ public class UDPServer {
     String strAddr = packet.getAddress().getHostAddress(); // client IP
     int iPort = packet.getPort(); // client port
 
-    System.out.println("Receiving: " + new String(data) + " (" + packet.getLength() + " bytes)" +" from client at " + strAddr + " on port " + iPort);
+    String log = "Receiving: " + new String(data) + " (" + packet.getLength() + " bytes)" +" from client at " + strAddr + " on port " + iPort;
+    System.out.println(log);
+
+    try {
+      FileWriter fw = new FileWriter(args[1]);
+      PrintWriter pw = new PrintWriter(fw);
+      pw.println(log);
+      pw.close();
+    } catch(IOException e) {
+      System.out.println(e);
+    }
 
     packet.setData(data);
     socket.send(packet);
