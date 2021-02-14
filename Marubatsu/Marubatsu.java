8f4a667a7eb99cbeb842921e96d18db9be3f052a
codeaid-dev <codeaid.lab@gmail.com>
2021-02-14 13:28:29 +0900

    add

---
+++ Marubatsu/Marubatsu.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Marubatsu {
+  public static void main(String[] args) {
+    System.out.print("横数と縦数：");
+    Scanner scan = new Scanner(System.in);
+    String str = scan.nextLine();
+    String[] size = str.split(",", 0);
+    int width = Integer.parseInt(size[0]);
+    int height = Integer.parseInt(size[1]);
+    String[] marubatsu = {"○", "✕"};
+    int order = 1;
+
+    for(int i = 0; i < height; i++) {
+      String ch = "";
+      order = 1 - order;
+      for(int j = 0; j < width; j++) {
+        if(j % 2 == 0) {
+          ch += marubatsu[order];
+        } else {
+          ch += marubatsu[1 - order];
+        }
+      }
+      System.out.println(ch);
+    }
+    scan.close();
+  }
+}
---
+++ NumberOfTypes/NumberOfTypes.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class NumberOfTypes {
+  public static void main(String[] args) {
+    System.out.print("入力：");
+    Scanner scan = new Scanner(System.in);
+    String str = scan.nextLine();
+    str = str.trim();
+
+    int count = 0;
+    while (str.length() > 0) {
+      String w = str.substring(0, 1);
+      count++;
+      str = str.replace(w, "");
+    }
+    System.out.println("種類数：" + count);
+    scan.close();
+  }
+}
