5a1e08da325b8319601d9a16a24639214bb0d89d
codeaid-dev <codeaid.lab@gmail.com>
2021-02-17 16:13:59 +0900

    add

---
+++ Password/Password.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Password {
+  public static void main(String[] args) {
+    System.out.print("パスワード桁数：");
+    Scanner scan = new Scanner(System.in);
+    String str = scan.nextLine();
+    str = str.trim();
+    int wc = Integer.parseInt(str);
+    String[] ascii = {"a", "b", "c", "d", "E", "F", "G", "H", "0", "1", "2", "3", "4", "!", "@", "#", "$", "%"};
+
+    if (wc < 4 || wc > 16) {
+      System.out.println("4~16までの桁数を入力してください");
+      scan.close();
+      return;
+    }
+
+    ArrayList<String> password = new ArrayList<String>();
+    for (int i = 0; i < wc; i++) {
+      int index = new java.util.Random().nextInt(ascii.length);
+      password.add(ascii[index]);
+    }
+
+    String result = "";
+    for (String pass : password) {
+      result += pass;
+    }
+    System.out.println("password: " + result);
+    scan.close();
+  }
+}
---
+++ PasswordEx/PasswordEx.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PasswordEx {
+  public static void main(String[] args) {
+    System.out.print("パスワード桁数：");
+    Scanner scan = new Scanner(System.in);
+    String str = scan.nextLine();
+    str = str.trim();
+    int wc = Integer.parseInt(str);
+    int ascii_s = 33;
+    int ascii_e = 126;
+
+    if (wc < 8 || wc > 32) {
+      System.out.println("8~32までの桁数を入力してください");
+      scan.close();
+      return;
+    }
+
+    ArrayList<String> password = new ArrayList<String>();
+    for (int i = 0; i < wc; i++) {
+      int index = new java.util.Random().nextInt(ascii_e-ascii_s)+ascii_s;
+      password.add(Character.toString((char)index));
+    }
+
+    String result = "";
+    for (String pass : password) {
+      result += pass;
+    }
+    System.out.println("password: " + result);
+    scan.close();
+  }
+}
