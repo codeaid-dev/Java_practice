1e8f9d12e2d54c7e12ae2dc602a8400cddbcfe57
codeaid-dev <codeaid.lab@gmail.com>
2022-08-05 23:23:36 +0900

    add

---
+++ Exercise_var/Ex11.java
@@ -0,0 +1,12 @@
+package Exercise_var;
+
+public class Ex11 {
+  public static void main(String[] args) {
+    int up=20, bottom=35, height=30;
+    int tri = bottom*height/2;
+    int tra = (up+bottom)*height/2;
+    int rec = bottom*height;
+    int cir = (int)((height/2)*(height/2)*3.14);
+    System.out.println("総面積は"+(tri+tra+rec+cir)+"平方cmです");
+  }
+}
---
+++ Exercise_var/Ex17.java
@@ -0,0 +1,12 @@
+package Exercise_var;
+
+public class Ex17 {
+  public static void main(String[] args) {
+    System.out.print("入力 >>");
+    String s = new java.util.Scanner(System.in).nextLine();
+    System.out.print("オフセット >>");
+    int n = new java.util.Scanner(System.in).nextInt();
+    System.out.println(s);
+    System.out.println(s.substring(n));
+  }
+}
