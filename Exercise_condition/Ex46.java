305a2a656501f6f82930965e629d6a2f83b43a5a
codeaid-dev <codeaid.lab@gmail.com>
2023-03-12 11:42:56 +0900

    fix

--- Exercise_condition/Ex46.java
+++ Exercise_condition/Ex46.java
@@ -1,6 +1,18 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ex46 {
   public static void main(String[] args) {
     int[] nums = {10,20,30,20,10,50,60,40,80,50,0,40};
+    List<String> res = new ArrayList<String>();
+    for (int n : nums) {
+      if (!res.contains(Integer.toString(n)) && n!=0) {
+        res.add(Integer.toString(n));
+      }
+    }
+    String res2 = String.join(" ",res);
+    System.out.println(res2);
     String result = "";
     for (int n : nums) {
       if (result.indexOf(Integer.toString(n)) == -1 && n != 0) {
