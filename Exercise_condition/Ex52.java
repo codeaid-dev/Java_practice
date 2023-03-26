ae2b3935ea6eb39b849ab644281d1e6d9abf2922
codeaid-dev <codeaid.lab@gmail.com>
2023-03-26 11:48:13 +0900

    fix

--- Exercise_condition/Ex52.java
+++ Exercise_condition/Ex52.java
@@ -2,7 +2,7 @@ public class Ex52 {
   public static void main(String[] args) {
     String[] uranai = {"今日は最高！","今日はそこそこです","今日はまぁまぁ","今日は最悪・・・"};
     for (int i=0; i<uranai.length; i++) {
-      int r = new java.util.Random().nextInt(uranai.length-1);
+      int r = new java.util.Random().nextInt(uranai.length);
       String w = uranai[i];
       uranai[i] = uranai[r];
       uranai[r] = w;
