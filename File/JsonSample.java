21649a9d61e3a9ed8097ce3fddabc60ace5677e2
codeaid-dev <codeaid.lab@gmail.com>
2024-02-16 13:53:10 +0900

    add

---
+++ File/JsonSample.java
@@ -0,0 +1,59 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+public class JsonSample {
+  public static void main(String[] args) {
+    //String script = "{\"Yamada\":\"abcde\"}";
+    System.out.print("ユーザー名 >>");
+    String username = new java.util.Scanner(System.in).nextLine();
+    System.out.print("パスワード >>");
+    String password = new java.util.Scanner(System.in).nextLine();
+
+    ObjectMapper mapper = new ObjectMapper();
+    mapper.enable(SerializationFeature.INDENT_OUTPUT);
+    File json = new File("sample.json");
+    if (json.exists()) { //ファイルが存在する
+      /* try {
+          User user = mapper.readValue(script, User.class);
+          System.out.println(user.username);
+      } catch (IOException e) {
+          e.printStackTrace();
+      } */
+
+      try {
+        JsonNode root = mapper.readTree(json);
+        Iterator<String> fieldNames = root.fieldNames();
+        while (fieldNames.hasNext()) {
+            String fieldName = fieldNames.next();
+            System.out.println(fieldName);
+            JsonNode node = root.get(fieldName);
+            System.out.println(node.toString());
+        }
+        ObjectNode onode = root.deepCopy();
+        onode.put(username, password);
+        mapper.writeValue(json, onode);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    } else { //ファイルがない
+      try {
+        ObjectNode root = mapper.createObjectNode();
+        root.put(username, password);
+        mapper.writeValue(json, root);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+}
+/*
+class User {
+  public String username;
+  public String password;
+}
+*/
