d155f423c440b42f7e4560a81303873cfa569db6
codeaid-dev <codeaid.lab@gmail.com>
2024-10-18 17:37:33 +0900

    fix

---
+++ Basic/InputName.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class InputName {
+  public static void main(String[] args) {
+    String name = new Scanner(System.in).nextLine();
+    System.out.println("Hello, " + name);
+  }
+}
