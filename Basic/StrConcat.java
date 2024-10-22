40a35e65a27b81c97418341957711194a603cec4
codeaid-dev <codeaid.lab@gmail.com>
2024-10-22 11:24:17 +0900

    add

---
+++ Basic/StrConcat.java
@@ -0,0 +1,8 @@
+public class StrConcat {
+  public static void main(String[] args) {
+    StringBuilder str = new StringBuilder("");
+    str.append("aiueo");
+    str.append("あいうえお");
+    System.out.println(str.toString());
+  }
+}
