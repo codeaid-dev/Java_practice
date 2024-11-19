672e2be2e2dbb81f38a8d5036fc2937b098e7c1b
codeaid-dev <codeaid.lab@gmail.com>
2024-11-19 13:32:44 +0900

    fix

--- Condition/IfStrSample.java
+++ Condition/IfStrSample.java
@@ -8,10 +8,14 @@ public class IfStrSample {
     }
     String str1 = "Good morning";
     String str2 = "Good ";
+    String str3 = "Good morning";
     if (str1 == str2 + "morning") {
       System.out.println("==で比較し同じ");
     } else if (str1.equals(str2 + "morning")) {
       System.out.println("equals()で比較し同じ");
+    }
+    if (str1 == str3) {
+      System.out.println("==で比較し同じ");
     }
   }
 }
