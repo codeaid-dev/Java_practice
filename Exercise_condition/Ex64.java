794963ab12681f33de1d07a3473d69d98037d47b
codeaid-dev <codeaid.lab@gmail.com>
2024-12-07 18:52:32 +0900

    fix rename

--- Exercise_condition/Ex64.java
+++ Exercise_condition/Ex64.java
@@ -1,33 +1,39 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Ex64 {
   public static void main(String[] args) {
-    int[][] score = {{9,1},{8,2},{10},{5,0},{3,6},{4,2},{7,3},{6,3},{10},{9,1,0}};
-    int result=0, next1=0, next2=0, next3=0;
-    for (int i=score.length-1; i>=0; i--) {
-      int[] frame = score[i];
-      int total = 0;
-      for (int j=0; j<frame.length; j++) { // 各フレームの合計
-        total += frame[j];
-      }
-      if (frame.length == 1) { // ストライクの時は10と次とその次のスコアを加算する
-        result += 10+next1+next2;
-        next2=next1;
-        next1=10;
-      } else if (frame.length == 2 && total == 10) { // スペアの時は10と次のスコアを加算する
-        result += 10 + next1;
-        next1=frame[0];
-        next2=frame[1];
-      } else { // 通常時はそのスコアだけを加算する
-        result += total;
-        if (frame.length == 3) {
-          next1=frame[0];
-          next2=frame[1];
-          next3=frame[2];
-        } else {
-            next1=frame[0];
-            next2=frame[1];
-        }
+    int result = 0;
+    for (int i=1; i<=3; i++) {
+      System.out.print(i + "回目：");
+      new Scanner(System.in).nextLine();
+      result += slot();
+    }
+    System.out.println("合計：" + result);
+  }
+
+  public static int slot() {
+    String[] rool = {"●","■","▲"};
+    int[] count = {0,0,0};
+    String[] slot = {"","",""};
+    for (int i=0; i<3; i++) {
+      int num = new Random().nextInt(3);
+      count[num] += 1;
+      slot[i] = rool[num];
+    }
+    System.out.println(String.join(" ", slot));
+
+    int score = 0;
+    for (int i=0; i<rool.length; i++) {
+      if (i == 0) {
+        if (count[i] == 3) { score = 100; }
+      } else if (i == 1) {
+        if (count[i] == 3) { score = 200; }
+      } else {
+        if (count[i] == 3) { score = 300; }
       }
     }
-    System.out.println("スコア"+result+"点");
+    System.out.println(score);
+    return score;
   }
 }
