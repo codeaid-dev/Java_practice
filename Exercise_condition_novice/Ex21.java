2b2b224f50af6d8f3712d76268f1b28f11da9663
codeaid-dev <codeaid.lab@gmail.com>
2024-12-14 22:53:18 +0900

    add

---
+++ Exercise_condition_novice/Ex21.java
@@ -0,0 +1,14 @@
+package Exercise_condition_novice;
+
+public class Ex21 {
+  public static void main(String[] args) {
+    int heads = 35;
+    int legs = 94;
+    for (int rabbits = 0; rabbits <= heads; rabbits++) {
+      int chikens = heads-rabbits;
+      if (2*chikens+4*rabbits == legs) {
+        System.out.println("兎は" + rabbits + "匹、鶏は" + chikens + "羽");
+      }
+    }
+  }
+}
---
+++ Exercise_condition_novice/Ex22.java
@@ -0,0 +1,13 @@
+package Exercise_condition_novice;
+
+public class Ex22 {
+  public static void main(String[] args) {
+    int pay = 10000;
+    int point = 0, month = 0;
+    while (point <= 20000) {
+      month += 1;
+      point += pay * 0.05;
+    }
+    System.out.println("ポイント金額が20000円を超えるのは" + month + "ヶ月後");
+  }
+}
