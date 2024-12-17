a8f6c74fe2e987a6d9a5fe69f57b3364cc7eaaf5
codeaid-dev <codeaid.lab@gmail.com>
2024-12-17 11:00:56 +0900

    add

---
+++ Exercise_array_novice/Ex13.java
@@ -0,0 +1,16 @@
+public class Ex13 {
+  public static void main(String[] args) {
+    char[] data = {'L', 'E', 'V', 'E', 'L'};
+    //char[] data = {'し', 'ん', 'ぶ', 'ん', 'し'};
+    boolean result = true;
+    for (int i = 0; i < data.length / 2; i++) {
+      if (data[i] != data[data.length - 1 - i]) {
+        result = false;
+        break;
+      }
+    }
+    if (result) System.out.println("回文です");
+    else System.out.println("回文ではありません");
+    //System.out.println(result ? "回文です" : "回文ではありません");
+  }
+}
