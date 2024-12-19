eca3bbbc63fee91498a143c4cb307d2e1ef77efa
codeaid-dev <codeaid.lab@gmail.com>
2024-12-19 10:23:30 +0900

    add

--- Exercise_array_novice/Ex17.java
+++ Exercise_array_novice/Ex17.java
@@ -1,15 +1,23 @@
-import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Ex17 {
   public static void main(String[] args) {
-    int[][] data = new int[13][4];
-    int number = 1;
-    for (int i = 0; i < data.length; i++) {
-      for (int j = 0; j < data[0].length; j++) {
-        data[i][j] = number;
-      }
-      number++;
+    int wc = 0;
+    while (true) {
+      System.out.print("パスワード桁数：");
+      wc = new Scanner(System.in).nextInt();
+      if (wc < 4 || wc > 16) {
+        System.out.println("4~16までの桁数を入力してください");
+        continue;
+      } else break;
     }
-    System.out.println(Arrays.deepToString(data));
+    String[] ascii = {"a", "b", "c", "d", "E", "F", "G", "H", "0", "1", "2", "3", "4", "!", "@", "#", "$", "%"};
+    String result = "";
+    for (int i = 0; i < wc; i++) {
+      int index = new Random().nextInt(ascii.length);
+      result += ascii[index];
+    }
+    System.out.println("password: " + result);
   }
 }
