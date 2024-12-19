42e4ebbee31ace83dba4df39df5743381ead4638
codeaid-dev <codeaid.lab@gmail.com>
2024-12-19 23:03:15 +0900

    add

---
+++ Exercise_method_novice/Ex1.java
@@ -0,0 +1,8 @@
+public class Ex1 {
+  public static void main(String[] args) {
+    printHello();
+  }
+  public static void printHello() {
+    System.out.println("Hello, World!");
+  }
+}
---
+++ Exercise_method_novice/Ex2.java
@@ -0,0 +1,8 @@
+public class Ex2 {
+  public static void main(String[] args) {
+    greet("山田");
+  }
+  public static void greet(String name) {
+    System.out.println("Hello, " + name + "!");
+  }
+}
