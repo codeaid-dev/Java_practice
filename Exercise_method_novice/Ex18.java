4edd4fe834c6cecf664c28d3e8731c966ca19733
codeaid-dev <codeaid.lab@gmail.com>
2024-12-20 15:39:37 +0900

    add

---
+++ Exercise_method_novice/Ex18.java
@@ -0,0 +1,12 @@
+public class Ex18 {
+  public static void main(String[] args) {
+    System.out.println("結果：" + factorial(5));
+  }
+  public static int factorial(int n) {
+    int result = 1;
+    for (int i = 1; i <= n; i++) {
+      result *= i;
+    }
+    return result;
+  }
+}
---
+++ Exercise_method_novice/Ex20.java
@@ -0,0 +1,13 @@
+public class Ex20 {
+  public static void main(String[] args) {
+    System.out.println("結果：" + isPrime(17));
+    System.out.println("結果：" + isPrime(18));
+  }
+  public static boolean isPrime(int num) {
+    if (num <= 1) return false;
+    for (int i = 2; i < num; i++) {
+      if (num % i == 0) return false;
+    }
+    return true;
+  }
+}
