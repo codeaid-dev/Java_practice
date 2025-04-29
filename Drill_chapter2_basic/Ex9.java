9975bb80d4528100b86f484ade53a0bb9ed5341e
codeaid-dev <codeaid.lab@gmail.com>
2025-04-29 10:43:54 +0900

    fix

--- Drill_chapter2_basic/Ex9.java
+++ Drill_chapter2_basic/Ex9.java
@@ -1,8 +1,8 @@
 public class Ex9 {
   public static void main(String[] args) {
-    System.out.print("右辺入力 >>");
-    int left = new java.util.Scanner(System.in).nextInt();
     System.out.print("左辺入力 >>");
+    int left = new java.util.Scanner(System.in).nextInt();
+    System.out.print("右辺入力 >>");
     int right = new java.util.Scanner(System.in).nextInt();
     System.out.println(left+"割る"+right+"は、"+left/right+"余り"+left%right+"です");
   }
