13a9ad378211fc4fa4474684b8d794e790c893e8
codeaid-dev <codeaid.lab@gmail.com>
2025-06-15 15:59:24 +0900

    add

---
+++ GUI/MultiEvent.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class MultiEvent {
+  public static void main(String[] args) {
+    JFrame frame = new JFrame("複数イベント");
+    JButton start = new JButton("スタート");
+    JPanel panel1 = new JPanel();
+    JLabel label1 = new JLabel("スタートを押して始めてください", JLabel.CENTER);
+    JLabel label2 = new JLabel("---", JLabel.CENTER);
+    frame.add(label1, BorderLayout.NORTH);
+    panel1.add(start);
+    frame.add(panel1, BorderLayout.CENTER);
+    frame.add(label2, BorderLayout.SOUTH);
+    frame.setSize(200, 100);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setVisible(true);
+
+    int delay = 1000;
+    Timer[] timer = new Timer[1];
+    ActionListener drum = new ActionListener() {
+      int count = 0;
+      public void actionPerformed (ActionEvent evt) {
+        if (evt.getSource() == start) {
+          label2.setText("ボタンが押された");
+          count = 0;
+          timer[0].start();
+        } else {
+          count++;
+          label2.setText(""+count);
+        }
+
+      }
+    };
+    timer[0] = new Timer(delay, drum);
+    start.addActionListener(drum);
+  }
+}
---
+++ GUI/SlotMoji.java
@@ -0,0 +1,76 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class SlotMoji {
+  public static void main(String[] args) {
+    JFrame frame = new JFrame("スロットマシン");
+    JLabel slot[] = new JLabel[3];
+    JButton btn[] = new JButton[3];
+    JButton start = new JButton("スタート");
+    Timer timer[] = new Timer[1];
+    boolean[] status = {true, true, true};
+
+    JLabel label1 = new JLabel("スタートを押して始めてください", JLabel.CENTER);
+    frame.add(label1, BorderLayout.NORTH);
+
+    JPanel panel1 = new JPanel();
+    int num1 = (int)(Math.random() * 10);
+    slot[0] = new JLabel(String.valueOf(num1));
+    int num2 = (int)(Math.random() * 10);
+    slot[1] = new JLabel(String.valueOf(num2));
+    int num3 = (int)(Math.random() * 10);
+    slot[2] = new JLabel(String.valueOf(num3));
+    slot[0].setFont(new Font("Arial", Font.PLAIN, 80));
+    slot[1].setFont(new Font("Arial", Font.PLAIN, 80));
+    slot[2].setFont(new Font("Arial", Font.PLAIN, 80));
+    panel1.add(slot[0]);
+    panel1.add(slot[1]);
+    panel1.add(slot[2]);
+    frame.add(panel1, BorderLayout.CENTER);
+
+    int delay = 200;
+    ActionListener drum = new ActionListener() {
+      public void actionPerformed (ActionEvent evt) {
+        if (evt.getSource() == start) {
+          timer[0].start();
+          for (int i=0; i < status.length; i++) {
+            status[i] = false;
+          }
+        }
+
+        for (int i = 0; i < btn.length; i++) {
+          if (evt.getSource() == btn[i]) {
+            status[i] = true;
+          }
+          if (status[i] == false) {
+            int num = Integer.parseInt(slot[i].getText());
+            if (++num >= 10)
+              num = 0;
+            slot[i].setText(String.valueOf(num));
+          }
+        }
+      }
+    };
+    timer[0] = new Timer(delay, drum);
+    start.addActionListener(drum);
+    JPanel panel2 = new JPanel();
+    panel2.add(start);
+
+    btn[0] = new JButton("ストップ");
+    btn[0].addActionListener(drum);
+    btn[1] = new JButton("ストップ");
+    btn[1].addActionListener(drum);
+    btn[2] = new JButton("ストップ");
+    btn[2].addActionListener(drum);
+    panel2.add(btn[0]);
+    panel2.add(btn[1]);
+    panel2.add(btn[2]);
+
+    frame.add(panel2, BorderLayout.SOUTH);
+
+    frame.setSize(500, 200);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setVisible(true);
+  }
+}
