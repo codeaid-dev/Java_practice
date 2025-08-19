59854cbb710c5bd00949f0ebd57f8b1a20fc70c4
codeaid-dev <codeaid.lab@gmail.com>
2025-08-19 13:28:03 +0900

    fix

--- DateTime_drill/Ex10.java
+++ DateTime_drill/Ex10.java
@@ -6,9 +6,9 @@ public class Ex10 {
   public static void main(String[] args) {
     DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy年MM月dd日 HH時mm分");
     ZonedDateTime tokyo = ZonedDateTime.now(ZoneId.of("Asia/Tokyo"));
-    ZonedDateTime newyork = tokyo.withZoneSameInstant(ZoneId.of("America/Los_Angeles"));
+    ZonedDateTime los = tokyo.withZoneSameInstant(ZoneId.of("America/Los_Angeles"));
     String s1 = tokyo.format(dtf);
-    String s2 = newyork.format(dtf);
+    String s2 = los.format(dtf);
     System.out.println("東京の日時："+s1);
     System.out.println("ロサンゼルスの日時："+s2);
   }
