60515bbb7898595ab958489a5f8e43a89db07303
codeaid-dev <codeaid.lab@gmail.com>
2026-06-25 15:00:10 +0900

    fix

--- Collection_drill/Ex37.java
+++ Collection_drill/Ex37.java
@@ -1,20 +1,19 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.HashSet;
-import java.util.Set;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Ex37 {
   public static void main(String[] args) {
     List<Integer> nums = new ArrayList<>(List.of(1,2,2,2,4,4,4,5,5,5,5));
-    Set<Integer> set = new HashSet<>(nums);
-    System.out.println(set.toString());
+    List<Integer> newNums = new ArrayList<>(new HashSet<>(nums));
+    System.out.println(newNums.toString());
     Map<Integer,Integer> result = new HashMap<>();
-    for (Integer n : set) {
+    for (Integer n : newNums) {
       int count=0;
       for (Integer i : nums) {
-        if (n.equals(i)) {
+        if (n == i) {
           count++;
         }
       }
